a0373fc1b8d2615cca329d88945d74c00ced8ac7
vitorfacioli <vitorfacioli@hotmail.com>
2020-06-13 23:16:31 -0300

    Modal visualização reserva

--- src/screen/reservas/index.js
+++ src/screen/reservas/index.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, TouchableOpacity, FlatList} from 'react-native';
+import {View, Text, TextInput, FlatList} from 'react-native';
+import Modal from 'react-native-modal';
 import styles from './styles';
-import {Icon_porco} from '../../assets/svg/icons';
 import Qg from './components/qg';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {fonts} from '../../assets';
 
 export default class QGs extends Component {
   constructor(props) {
@@ -11,12 +13,13 @@ export default class QGs extends Component {
       data: [
         {
           img: 'https://i.ytimg.com/vi/t3EbiYDFvog/maxresdefault.jpg',
-          nome: 'QG 32',
+          nome: 'QG 32 A',
           km: 3,
           endereco: 'Rodovia Luiz de Queiroz, KM 33, Americana',
           disponiveis: 2,
         },
       ],
+      showVisualizar: false,
     };
   }
 
@@ -39,7 +42,7 @@ export default class QGs extends Component {
           renderItem={({item: e}) => (
             <Qg
               data={e}
-              goTo={() => this.props.navigation.navigate('Detalhes_qg')}
+              showModal={() => this.setState({showVisualizar: true})}
             />
           )}
           keyExtractor={(e) => e.img}
@@ -49,6 +52,54 @@ export default class QGs extends Component {
             </Text>
           )}
         />
+
+        <Modal isVisible={this.state.showVisualizar} style={{margin: 0}}>
+          <View style={styles.containerModal}>
+            <Text style={styles.titleModal}>QG 32 A | 3 km</Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 20,
+              }}>
+              <View>
+                <Text style={styles.titleCheck}>Previsão de entrada</Text>
+                <Text>30/06/2020 18h00</Text>
+              </View>
+
+              <View>
+                <Text style={styles.titleCheck}>Previsão de saída</Text>
+                <Text>01/07/2020 06h00</Text>
+              </View>
+            </View>
+
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => this.props.navigation.navigate('Check_in')}>
+                <Text style={styles.moedas}>Check-in</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => this.props.navigation.navigate('Check_out')}>
+                <Text style={styles.moedas}>Check-out</Text>
+              </TouchableOpacity>
+            </View>
+
+            <Text
+              style={{
+                marginTop: 50,
+                color: 'red',
+                fontFamily: fonts.bold,
+                fontSize: 22,
+                textDecorationLine: 'underline',
+              }}>
+              Cancelar reserva
+            </Text>
+          </View>
+        </Modal>
       </View>
     );
   }
